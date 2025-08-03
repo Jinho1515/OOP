c19c6bf59afc4e52b8c836f44856a14381c08c27
Jinho1515 <a1957020@adelaide.edu.au>
2025-08-03 15:03:49 +0930

    CORRECTING INCORRECT NAMING

--- main-3-5.cpp
+++ main-3-5.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 
-double weighted_average(int array[], int n);
+ double sum_even(double array[], int n);
 
 
 int main() {
-    int arr[] = {1,5,10,6,7,8,10};
+    double arr[] = {1,5,10,6,7,8,10};
     int n = sizeof(arr) / sizeof(arr[0]);
     
 
-    double result = weighted_average(arr, n);
+    double result = sum_even(arr, n);
     
     std::cout << "The sum of even array is " << result << "." << std::endl;
 
