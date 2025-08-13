ed0b38dfe53448b4d48830579e9fcbe182c07f0f
Jinho1515 <a1957020@adelaide.edu.au>
2025-08-13 20:33:21 +0930

    workshop

---
+++ workshop.h
@@ -0,0 +1,47 @@
+
+#include <iostream>
+
+void changeValue(double* p) {
+  if (p) {
+    *p = 42.0;
+  }
+}
+
+void printArray(double* a, int n) {
+  for (int i = 0; i < n; i++) {
+    std::cout << a[i];
+  }
+  std::cout << '\n';
+}
+
+double arrayMax(double* a, int n) {
+  double maxVal = a[0];
+  for (int i = 0; i < n; i++) {
+    if (maxVal > a[i]) {
+      maxVal = a[i];
+    }
+  }
+
+  return maxVal;
+}
+
+double* dynamicArray(int N) {
+  if (N <= 0) {
+    return nullptr;
+  }
+  double* arr = new double[N];
+  for (int i = 0; i < N; ++i) {
+    arr[i] = static_cast<double>(i);
+  }
+  return arr;
+}
+double* dynamicArray(int N, double M) {
+  if (M <= 0) {
+    return nullptr;
+  }
+  double* arr = new double[N];
+  for (int i = 0; i < N; ++i) {
+    arr[i] = M;
+  }
+  return arr;
+}
--- ws2_and_practical1/main-3-4.cpp
+++
@@ -1,9 +0,0 @@
-void print_pass_fail(char grade);
-
-
-int main() {
-  print_pass_fail('A'); 
-  print_pass_fail('D'); 
-  print_pass_fail('X'); 
-  return 0;
-}
