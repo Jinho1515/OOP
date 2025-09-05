f9e90dde39fbd1b8bc6f2e3b8da9f3c21e2a9050
Jinho1515 <a1957020@adelaide.edu.au>
2025-09-05 21:38:54 +0930

    corrction

--- Vehicle.h
+++ Vehicle.h
@@ -9,7 +9,6 @@ private:
     int ID;
 
 protected:
-
     int secondsSinceEntry() const;
 
 public:
@@ -17,27 +16,7 @@ public:
     virtual ~Vehicle();
 
     int getID() const;
-
     virtual int getParkingDuration() const = 0;
-};
-
-
-class Car : public Vehicle {
-public:
-    explicit Car(int id);
-    int getParkingDuration() const override;
-};
-
-class Bus : public Vehicle {
-public:
-    explicit Bus(int id);
-    int getParkingDuration() const override;
-};
-
-class Motorbike : public Vehicle {
-public:
-    explicit Motorbike(int id);
-    int getParkingDuration() const override;
 };
 
 #endif // VEHICLE_H
